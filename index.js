6c6c5f83c2ec278832add1c3a15d307c1a97d454
Mya <35534673+myawilliams11@users.noreply.github.com>
2018-05-25 11:12:18 -0400

    functioning, missing picture

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+'use strict';
 var boxtop=200;
 var boxleft=200;
 
-'use strict';
-
 document.addEventListener('keydown', (event) => {
   const keyName = event.key;
-  alert('keydown event\n\n' + 'key: ' + keyName);
+    //   alert('keydown event\n\n' + 'key: ' + keyName);
+
+    if(keyName === "ArrowDown") {
+        boxtop += 10;  
+    } else if (keyName === "ArrowUp") {
+        boxtop -= 10;
+    } else if (keyName === "ArrowRight"){
+        boxleft += 10;
+    } else if (keyName === "ArrowLeft") {
+        boxleft -= 10;
+    }
+
+
+    document.getElementById("box").style.top = boxtop + "px";
+
+    document.getElementById("box").style.left = boxleft + "px";
 });
+
