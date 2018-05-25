e3ebfb11859ee47c432bc263f84c0b67a7bf4d06
Mya <35534673+myawilliams11@users.noreply.github.com>
2018-05-25 10:47:46 -0400

    First

---
+++ index.js
@@ -0,0 +1,9 @@
+var boxtop=200;
+var boxleft=200;
+
+'use strict';
+
+document.addEventListener('keydown', (event) => {
+  const keyName = event.key;
+  alert('keydown event\n\n' + 'key: ' + keyName);
+});
